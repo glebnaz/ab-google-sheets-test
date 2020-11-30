a5200b91cf7c0941c118804fd877e2552e54306f
Gleb Nazemnov <nazemnovgleb@gmail.com>
2020-11-30 16:40:09 +0400

    Add range string func

--- sheets/range.go
+++ sheets/range.go
@@ -1,8 +1,21 @@
 package sheets
 
+import "fmt"
+
 //Range is range for sheets
 type Range struct {
 	List  string
 	Start string
 	End   string
+}
+
+func (r Range) GetRangeString() (string, error) {
+	if len(r.Start) == 0 || len(r.End) == 0 {
+		return "", fmt.Errorf("Empty Start or End range field")
+	}
+	if len(r.List) == 0 {
+		return fmt.Sprintf("%s:%s", r.Start, r.End), nil
+	} else {
+		return fmt.Sprintf("%s!%s:%s", r.List, r.Start, r.End), nil
+	}
 }
