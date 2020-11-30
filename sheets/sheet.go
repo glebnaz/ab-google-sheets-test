c212bd7f1dc2c17eb9cdfe5695b85e9597b3dea1
Gleb Nazemnov <nazemnovgleb@gmail.com>
2020-11-30 13:56:48 +0400

    Add sheets implementations

---
+++ sheets/sheet.go
@@ -0,0 +1,21 @@
+package sheets
+
+import (
+	gsheets "google.golang.org/api/sheets/v4"
+)
+
+//Sheet is table from google sheets
+//this interface implemented
+type Sheet interface {
+	GetAllValues() (map[string]string, error)
+	GetRangeValues(range_ string) (map[string]string, error)
+}
+
+type sheet struct {
+	//id of table, you can find id in url when editing table
+	//https://docs.google.com/spreadsheets/d/15y-MClMIO6wtf6o2cj0lfa7Kn4GZmbok1yOBqWwDuDc/edit#gid=0
+	//for example in this url id is: 15y-MClMIO6wtf6o2cj0lfa7Kn4GZmbok1yOBqWwDuDc
+	id string
+	//srv is Service from google lib api
+	srv *gsheets.Service
+}
