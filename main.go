b7ea502ec9cf8633aec5832f6b0673a45a39218b
Gleb Nazemnov <nazemnovgleb@gmail.com>
2020-11-30 16:48:45 +0400

    Add auth

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/glebnaz/ab-google-sheets-test/sheets"
+)
+
+func main() {
+	range_ := sheets.Range{
+		Start: "A1",
+		End:   "C3",
+		List:  "",
+	}
+	table, err := sheets.InitSheet("15y-MClMIO6wtf6o2cj0lfa7Kn4GZmbok1yOBqWwDuDc")
+	if err != nil {
+		panic(err)
+	}
+	v, err := table.GetValues(range_)
+	fmt.Println(v)
+	fmt.Println(err)
 }
